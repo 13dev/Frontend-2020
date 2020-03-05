f64340de9dd28e2f5f7142fbe19329d4b2ebcf62
13dev <qwerty124563@gmail.com>
2020-03-05 17:33:46 +0000

    Fix menu burger and change static files to assets

--- src/components/partials/Navbar.vue
+++ src/components/partials/Navbar.vue
@@ -1,48 +1,26 @@
 <template>
    <nav class="navbar" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
-         <a class="navbar-item" href="https://bulma.io">
-            <img :src="logoUrl" alt="Logo" />
+         <a class="navbar-item" href="http://apple.com">
+            <img src="@/assets/logo.png" alt="Logo" width="112" height="28" />
          </a>
 
-         <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+         <a @click="showNav = !showNav" :class="{ 'is-active' : showNav }" role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
          </a>
       </div>
 
-      <div id="navbarBasicExample" class="navbar-menu">
+      <div :class="{ 'is-active' : showNav }" class="navbar-menu">
          <div class="navbar-start">
             <a class="navbar-item">
                Home
             </a>
 
             <a class="navbar-item">
-               Documentation
+               Products
             </a>
-
-            <div class="navbar-item has-dropdown is-hoverable">
-               <a class="navbar-link">
-                  More
-               </a>
-
-               <div class="navbar-dropdown">
-                  <a class="navbar-item">
-                     About
-                  </a>
-                  <a class="navbar-item">
-                     Jobs
-                  </a>
-                  <a class="navbar-item">
-                     Contact
-                  </a>
-                  <hr class="navbar-divider">
-                  <a class="navbar-item">
-                     Report an issue
-                  </a>
-               </div>
-            </div>
          </div>
 
          <div class="navbar-end">
@@ -65,7 +43,7 @@
    export default {
       data() {
          return {
-            logoUrl: './src/static/logo.png'
+            showNav: false,
          }
       }
    }
